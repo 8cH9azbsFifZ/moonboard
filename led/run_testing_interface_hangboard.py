d9246044f7c1246e919284fa0b29f4659856a345
8cH9azbsFifZ <g@ziegenhain.com>
2022-01-12 20:16:25 +0100

    name

--- led/run_testing_interface_hangboard.py
+++ led/run_testing_interface_hangboard.py
@@ -143,8 +143,8 @@ class Database():
 
 
     def _record_data(self, hostname="localhost",port=1883):
-        logging.debug("Start recording data from mqtt to database")
-        self._client= paho.Client("client-001")  # FIXME
+        logging.debug("Start recording data from mqtt to moonboard led")
+        self._client= paho.Client("display_moonboard") 
         self._client.on_message=self._on_message
         self._client.connect(hostname,port,60)#connect
 
